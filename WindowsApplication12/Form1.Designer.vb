653123e19e6050639a0bb386ba6de1ef99022aa8
huseyin <hus_@yahoo.com>
2015-03-03 15:16:54 +0200

    03.03.2015: working copy

---
+++ WindowsApplication12/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
+        Me.tslStatus = New System.Windows.Forms.ToolStripStatusLabel
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip
+        Me.tsbFile = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsbNew = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsbOpen = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsbSave = New System.Windows.Forms.ToolStripMenuItem
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator
+        Me.tsbExit = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsbLevels = New System.Windows.Forms.ToolStripDropDownButton
+        Me.tsmiLevel1 = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsmiLevel2 = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsmiLevel3 = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsmiLevel4 = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsmiLevel5 = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsmiFile = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsmiNew = New System.Windows.Forms.ToolStripMenuItem
+        Me.tsmiOpen = New System.Windows.Forms.ToolStripMenuItem
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.imageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.StatusStrip1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tslStatus})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 316)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(444, 22)
+        Me.StatusStrip1.TabIndex = 0
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'tslStatus
+        '
+        Me.tslStatus.Name = "tslStatus"
+        Me.tslStatus.Size = New System.Drawing.Size(60, 17)
+        Me.tslStatus.Text = "Click Start"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbFile, Me.tsbLevels, Me.tsmiFile})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(444, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsbFile
+        '
+        Me.tsbFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbNew, Me.tsbOpen, Me.tsbSave, Me.ToolStripMenuItem1, Me.tsbExit})
+        Me.tsbFile.Name = "tsbFile"
+        Me.tsbFile.Size = New System.Drawing.Size(37, 25)
+        Me.tsbFile.Text = "File"
+        '
+        'tsbNew
+        '
+        Me.tsbNew.Name = "tsbNew"
+        Me.tsbNew.ShortcutKeys = System.Windows.Forms.Keys.F2
+        Me.tsbNew.Size = New System.Drawing.Size(152, 22)
+        Me.tsbNew.Text = "New"
+        '
+        'tsbOpen
+        '
+        Me.tsbOpen.Name = "tsbOpen"
+        Me.tsbOpen.ShortcutKeys = System.Windows.Forms.Keys.F3
+        Me.tsbOpen.Size = New System.Drawing.Size(152, 22)
+        Me.tsbOpen.Text = "Open"
+        '
+        'tsbSave
+        '
+        Me.tsbSave.Name = "tsbSave"
+        Me.tsbSave.ShortcutKeys = System.Windows.Forms.Keys.F4
+        Me.tsbSave.Size = New System.Drawing.Size(152, 22)
+        Me.tsbSave.Text = "Save"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(149, 6)
+        '
+        'tsbExit
+        '
+        Me.tsbExit.Name = "tsbExit"
+        Me.tsbExit.ShortcutKeyDisplayString = "Esc"
+        Me.tsbExit.Size = New System.Drawing.Size(152, 22)
+        Me.tsbExit.Text = "Exit"
+        '
+        'tsbLevels
+        '
+        Me.tsbLevels.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmiLevel1, Me.tsmiLevel2, Me.tsmiLevel3, Me.tsmiLevel4, Me.tsmiLevel5})
+        Me.tsbLevels.Name = "tsbLevels"
+        Me.tsbLevels.Size = New System.Drawing.Size(56, 22)
+        Me.tsbLevels.Text = "Level 1"
+        Me.tsbLevels.ToolTipText = "Levels"
+        '
+        'tsmiLevel1
+        '
+        Me.tsmiLevel1.Checked = True
+        Me.tsmiLevel1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.tsmiLevel1.Name = "tsmiLevel1"
+        Me.tsmiLevel1.Size = New System.Drawing.Size(110, 22)
+        Me.tsmiLevel1.Text = "Level 1"
+        '
+        'tsmiLevel2
+        '
+        Me.tsmiLevel2.Name = "tsmiLevel2"
+        Me.tsmiLevel2.Size = New System.Drawing.Size(110, 22)
+        Me.tsmiLevel2.Text = "Level 2"
+        '
+        'tsmiLevel3
+        '
+        Me.tsmiLevel3.Name = "tsmiLevel3"
+        Me.tsmiLevel3.Size = New System.Drawing.Size(110, 22)
+        Me.tsmiLevel3.Text = "Level 3"
+        '
+        'tsmiLevel4
+        '
+        Me.tsmiLevel4.Name = "tsmiLevel4"
+        Me.tsmiLevel4.Size = New System.Drawing.Size(110, 22)
+        Me.tsmiLevel4.Text = "Level 4"
+        '
+        'tsmiLevel5
+        '
+        Me.tsmiLevel5.Name = "tsmiLevel5"
+        Me.tsmiLevel5.Size = New System.Drawing.Size(110, 22)
+        Me.tsmiLevel5.Text = "Level 5"
+        '
+        'tsmiFile
+        '
+        Me.tsmiFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsmiNew, Me.tsmiOpen})
+        Me.tsmiFile.Name = "tsmiFile"
+        Me.tsmiFile.Size = New System.Drawing.Size(44, 25)
+        Me.tsmiFile.Text = "Help"
+        '
+        'tsmiNew
+        '
+        Me.tsmiNew.Name = "tsmiNew"
+        Me.tsmiNew.Size = New System.Drawing.Size(170, 22)
+        Me.tsmiNew.Text = "How to"
+        '
+        'tsmiOpen
+        '
+        Me.tsmiOpen.Name = "tsmiOpen"
+        Me.tsmiOpen.Size = New System.Drawing.Size(170, 22)
+        Me.tsmiOpen.Text = "About Find Match"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'imageList1
+        '
+        Me.imageList1.ImageStream = CType(resources.GetObject("imageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.imageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.imageList1.Images.SetKeyName(0, "Background.jpg")
+        Me.imageList1.Images.SetKeyName(1, "black.png")
+        Me.imageList1.Images.SetKeyName(2, "blue.png")
+        Me.imageList1.Images.SetKeyName(3, "brown.png")
+        Me.imageList1.Images.SetKeyName(4, "green.png")
+        Me.imageList1.Images.SetKeyName(5, "indigo.png")
+        Me.imageList1.Images.SetKeyName(6, "lime.png")
+        Me.imageList1.Images.SetKeyName(7, "orange.png")
+        Me.imageList1.Images.SetKeyName(8, "pink.png")
+        Me.imageList1.Images.SetKeyName(9, "purple.png")
+        Me.imageList1.Images.SetKeyName(10, "red.png")
+        Me.imageList1.Images.SetKeyName(11, "turquoise.png")
+        Me.imageList1.Images.SetKeyName(12, "white.png")
+        Me.imageList1.Images.SetKeyName(13, "yellow.png")
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(444, 338)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Find Match"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents tslStatus As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents tsbLevels As System.Windows.Forms.ToolStripDropDownButton
+    Friend WithEvents tsmiLevel1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsmiLevel2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsmiLevel3 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsmiLevel4 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsmiLevel5 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents imageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents tsbFile As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsbNew As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsbOpen As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsbSave As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents tsbExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsmiFile As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsmiNew As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents tsmiOpen As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ WindowsApplication12/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+    Dim SelectedOne As PictureBox : Dim TotalRow, TotalCol As Integer : Dim ArrayImages() As Integer : Dim GameTime As Integer
+#Region "Least Important"
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Escape Then Call tsbExit_Click(sender, e)
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call tsmiLevel1_Click(sender, e)
+    End Sub
+    Private Sub tsbNew_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsbNew.Click
+        Dim PictureBoxes() As PictureBox : Dim TotalPictures As Integer = 0 'level işaretini değiştiriyor
+        tsmiLevel1.Checked = False : tsmiLevel2.Checked = False : tsmiLevel3.Checked = False : tsmiLevel4.Checked = False : tsmiLevel5.Checked = False
+        Select Case sender.ToString
+            Case "Level 2" : tsmiLevel2.Checked = True
+            Case "Level 3" : tsmiLevel3.Checked = True
+            Case "Level 4" : tsmiLevel4.Checked = True
+            Case "Level 5" : tsmiLevel5.Checked = True
+            Case Else : tsmiLevel1.Checked = True
+        End Select
+        For Each Pbox As Control In Me.Controls 'tüm picturebox kutularını bir diziye atıyor, ve sonra da siliyor
+            If Pbox.Name = "GamePictureBtn" Then ReDim Preserve PictureBoxes(TotalPictures) : PictureBoxes(TotalPictures) = Pbox : TotalPictures = TotalPictures + 1
+        Next '
+        For i = 0 To TotalPictures - 1
+            Me.Controls.Remove(PictureBoxes(i))
+        Next i
+        'yeni levela göre pictureboxları tekrar oluşturuyor
+        Call CreateImages(TotalRow, TotalCol) : Timer1.Enabled = True : GameTime = 0 : Call Timer1_Tick(sender, e)
+    End Sub
+    Private Sub tsbOpen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsbOpen.Click
+        'kaydet şu andaki oyunu
+    End Sub
+    Private Sub tsbSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsbSave.Click
+        'kayıtlı bir oyunu aç
+    End Sub
+    Private Sub tsbExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsbExit.Click
+        Me.Close() 'çıkış komutları
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim GameMin As String = Math.Floor(Convert.ToDouble(GameTime / 60)).ToString.PadLeft(2, "0")
+        Dim GameSec As String = Math.Floor(Convert.ToDouble(GameTime Mod 60)).ToString.PadLeft(2, "0")
+        tslStatus.Text = "Your time: " + GameMin + ":" + GameSec : GameTime += 1
+    End Sub
+    Private Sub tsmiLevel1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsmiLevel1.Click
+        TotalRow = 2 : TotalCol = 3 : Call tsbNew_Click(sender, e) 'rox*col çift olmak zorunda yoksa oyun bitmez
+    End Sub
+    Private Sub tsmiLevel2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsmiLevel2.Click
+        TotalRow = 2 : TotalCol = 4 : Call tsbNew_Click(sender, e) 'rox*col çift olmak zorunda yoksa oyun bitmez
+    End Sub
+    Private Sub tsmiLevel3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsmiLevel3.Click
+        TotalRow = 3 : TotalCol = 4 : Call tsbNew_Click(sender, e) 'rox*col çift olmak zorunda yoksa oyun bitmez
+    End Sub
+    Private Sub tsmiLevel4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsmiLevel4.Click
+        TotalRow = 3 : TotalCol = 6 : Call tsbNew_Click(sender, e) 'rox*col çift olmak zorunda yoksa oyun bitmez
+    End Sub
+    Private Sub tsmiLevel5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tsmiLevel5.Click
+        TotalRow = 4 : TotalCol = 6 : Call tsbNew_Click(sender, e) 'rox*col çift olmak zorunda yoksa oyun bitmez
+    End Sub
+#End Region
+#Region "Image Procedures"
+    Private Sub CreateImages(ByVal RowCount As Integer, ByVal ColCount As Integer)
+        Call CreateRndSeries(ColCount * RowCount)
+        Dim GamePictureBtn As PictureBox : Dim tmp As Integer = 0
+        For i = 0 To RowCount - 1
+            For j = 0 To ColCount - 1 'istenilen sıra ve sütun sayısına göre picturebox oluşturuyor
+                GamePictureBtn = New PictureBox : GamePictureBtn.Parent = Me : GamePictureBtn.Visible = True
+                GamePictureBtn.Name = "GamePictureBtn" : GamePictureBtn.Tag = CStr(ArrayImages(tmp)) : tmp += 1
+                GamePictureBtn.Size = New Size(90, 120) : GamePictureBtn.Top = 130 * i + 40 : GamePictureBtn.Left = 100 * j + 10
+                GamePictureBtn.BackgroundImage = imageList1.Images(0) : GamePictureBtn.BackgroundImageLayout = ImageLayout.Stretch
+                AddHandler GamePictureBtn.Click, AddressOf PictureBoxes_Click 'pictureboxların click eventi
+            Next j
+        Next i 'bundan sonra bir de dizi oluşturulacak. hangi resim hangi pictureboxta belli olsun diye
+        Me.Width = ColCount * 100 + 30 : Me.Height = RowCount * 130 + 100 'formun büyüklüğü düzeltiliyor
+    End Sub
+    Private Sub CreateRndSeries(ByVal Max_Number As Integer)
+        ReDim ArrayImages(Max_Number - 1) : Dim j As Integer = 0 : Dim tmp As Integer = Rnd() * (12 - (Max_Number / 2)) + 1
+        For i = 0 To Max_Number - 1 'burada pictureboxlarda gösterilecek resimler bir diziye aktarılıyor
+            ArrayImages(i) = j + tmp : If i Mod 2 = 1 Then j += 1
+        Next 've hayret edilecek birşey: dizideki elemanların sırası rastgele bir şekilde değişiyor
+        Shuffle(ArrayImages, 7) 'arrayimages disini 7 kere karıştır
+    End Sub
+    Private Sub Shuffle(ByRef arrayToBeShuffled As Array, ByVal numberOfTimesToShuffle As Integer)
+        Dim rndPosition As New Random(DateTime.Now.Millisecond)
+        For i As Integer = 1 To numberOfTimesToShuffle
+            For i2 As Integer = 1 To arrayToBeShuffled.Length
+                swap(arrayToBeShuffled(rndPosition.Next(0, arrayToBeShuffled.Length)), arrayToBeShuffled(rndPosition.Next(0, arrayToBeShuffled.Length)))
+            Next i2
+        Next i
+    End Sub
+    Private Sub swap(ByRef arg1 As Object, ByRef arg2 As Object) 'belirtilen iki değişkenin değerleri yer değiştiriliyor
+        Dim strTemp As String : strTemp = arg1 : arg1 = arg2 : arg2 = strTemp
+    End Sub
+    Private Sub PictureBoxes_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        If SelectedOne Is Nothing Then 'eğer ilk defa tıklanıyorsa
+            SelectedOne = sender : OpenImages(SelectedOne, CInt(SelectedOne.Tag)) 'tıklanan pictureboxu bir değişkene atar
+        Else 'ikince defaya tıklanıyorsa onu da başka bir değişkene at ve karşılaştırmalar yapar
+            Dim tmpName As PictureBox = sender
+            If tmpName.Equals(SelectedOne) Then Exit Sub
+            OpenImages(tmpName, CInt(tmpName.Tag)) : Me.Refresh() : System.Threading.Thread.Sleep(300)
+            If tmpName.Tag = SelectedOne.Tag Then
+                tmpName.Visible = False : SelectedOne.Visible = False 'eğer iki açılan resim de aynıysa ikisini de görünmeaz yapar
+                Call CheckEndofGame()
+            Else
+                OpenImages(tmpName, 0, SelectedOne, False)
+                'tmpName.BackgroundImage = imageList1.Images(0) : SelectedOne.BackgroundImage = imageList1.Images(0) 'eğer iki açılan resim aynıo değilse ikisini de kapat
+            End If
+            SelectedOne = Nothing
+        End If
+    End Sub
+    Private Sub CheckEndofGame() 'eğer tüm pictureboxların visible=false ise oyunu bitir
+        For Each Pbox As Control In Me.Controls
+            If Pbox.Name = "GamePictureBtn" Then If Pbox.Visible = True Then Exit Sub
+        Next
+        Timer1.Enabled = False : tslStatus.Text = "Game over" : MsgBox("Bravo! You have finished the game." & vbNewLine & tslStatus.Text)
+    End Sub
+    Private Sub OpenImages(ByVal varPicture As PictureBox, ByVal varImage As Integer, Optional ByVal varPicture2 As PictureBox = Nothing, Optional ByVal isEmpty As Boolean = True)
+        Dim tmpWidth As Integer = varPicture.Width : Dim tmpLeft As Integer = varPicture.Left
+        For i = tmpWidth To 0 Step -10 'resmi açarken boyutunu küçültüp büyütüyor:atraksiyon
+            If isEmpty = False Then varPicture2.Width -= 6 : varPicture2.Left += 3
+            varPicture.Width -= 6 : varPicture.Left += 3 : Me.Refresh()
+        Next
+        varPicture.BackgroundImage = imageList1.Images(varImage) : If isEmpty = False Then varPicture2.BackgroundImage = imageList1.Images(varImage)
+        For i = 0 To tmpWidth Step 10
+            If isEmpty = False Then varPicture2.Width += 6 : varPicture2.Left -= 3
+            varPicture.Width += 6 : varPicture.Left -= 3 : Me.Refresh()
+        Next
+    End Sub
+#End Region
+End Class
